9f3e43216680f9120cbc06c5ccefebabebc607dc
Jack Dagley <jack.dagley77@gmail.com>
2016-10-27 12:40:26 -0700

    add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+
+puts "What year were you born?"
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+puts "Would you like to enroll in the company’s health insurance?"
+

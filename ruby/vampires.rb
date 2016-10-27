b9f44d8dfbeb597b72a77344b2b9647f347ab085
Jack Dagley <jack.dagley77@gmail.com>
2016-10-27 14:32:02 -0700

    add logic for correct age

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,37 @@
+
+
 puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
 
 puts "What year were you born?"
+birth_year = gets.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+garlic_question = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to enroll in the company’s health insurance (y/n)?"
+health_insurance = gets.chomp
+
+
+#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+#If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+#Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+#Otherwise, print “Results inconclusive.”
+#Print the result at the end of the survey.
+
+#if age = true, AND (eats_garlic = true OR wants_insurance = true) print "probably a vampire"
+
+
+
+correct_age = false
+current_year = 2016
+if current_year - birth_year = age
+	correct_age = true
+	puts "correct age"
+end
+
 

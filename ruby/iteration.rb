595bf93771ed63fcb0cde3b69d6e0ea861ba18db
Jack Dagley <jack.dagley77@gmail.com>
2016-11-01 15:35:04 -0700

    answer #4 in last release

---
+++ ruby/iteration.rb
@@ -0,0 +1,131 @@
+def example
+	puts "red, green, blue"
+	yield("yellow", "turqoise")
+end
+
+example { |colors1, colors2| puts "my favorite colors are #{colors1} and #{colors2}" }
+
+
+cheese = ["cheddar", "bleu", "mozzarella", "gouda", "swiss", "parmesan"]
+
+crackers = {
+	wheat_thin: true,
+	cheese_its: false,
+	communion_crackers: false,
+	trisquits: true,
+	saltines: false,
+	mark_zuckerberg: true
+}
+
+=begin
+	
+Demonstrate that you can iterate through each one using .each, and then using .map! 
+(modifying the data in some way). Note that you can't call .map! on a hash, 
+so you can skip that (.map returns an array, so a destructive map method is 
+logically impossible on a hash). Print the data structures before and after each 
+call to demonstrate whether they have been modified or not.
+	
+=end
+p cheese
+p crackers
+
+puts "============="
+
+cheese.each do |type| 
+	puts type
+end
+
+puts "============="
+
+cap_cheese = cheese.map! do |type| 
+	puts type.capitalize
+end
+
+puts "============="
+
+crackers.each do |type|
+	puts type
+end
+
+puts "============="
+
+p cap_cheese
+p crackers
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.delete_if {|number| number.even?}
+puts numbers
+
+states_and_capitals = {
+	California: "Sacramento",
+	New_York: "Albany",
+	Texas: "Austin",
+	Nevada: "Reno"
+}
+
+states_and_capitals.delete_if {|state, capital| capital =~ /[A]/}
+puts states_and_capitals
+
+puts "============="
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.keep_if {|number| number.even?}
+puts numbers
+
+states_and_capitals = {
+	California: "Sacramento",
+	New_York: "Albany",
+	Texas: "Austin",
+	Nevada: "Reno"
+}
+
+states_and_capitals.keep_if {|state, capital| capital =~ /[A]/}
+
+puts states_and_capitals
+
+puts "============="
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.select! {|number| number > numbers[5]}
+puts numbers
+
+states_and_capitals = {
+	California: "Sacramento",
+	New_York: "Albany",
+	Texas: "Austin",
+	Nevada: "Reno"
+}
+
+states_and_capitals.keep_if {|state, capital| capital.length > 4}
+
+puts states_and_capitals
+
+puts "============="
+
+#A method that will take out items from the array or hash when it evaluates to true
+#and stop taking them out when the item in array or hash becomes false 
+#(you may not find a perfectly working option for the hash, and that's okay).
+
+def new_numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	numbers.drop_while {|number| number < 4}
+end
+
+puts new_numbers
+
+states_and_capitals = {
+	California: "Sacramento",
+	New_York: "Albany",
+	Texas: "Austin",
+	Nevada: "Reno"
+}
+
+states_and_capitals.delete_if {|state, capital| capital.length > 6}
+puts states_and_capitals
+
+
+
+

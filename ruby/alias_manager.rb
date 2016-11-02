3311a722d4868590a07b6746e47ec9eb5347d2c9
Jack Dagley <jack.dagley77@gmail.com>
2016-11-02 12:45:44 -0700

    add logic for name reverse

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -18,19 +18,22 @@ puts "what is the name of the spy?"
 real_name = gets.chomp
 
 #pass in a real name to the method, separate first and last name and turn into an array:
+#make the user input into an array by splitting the space between first and last names,
+#and then name it to make it more clear
+name_array = real_name.split(' ')
 
 
-def name_to_array(real_name)
-real_name.split(' ')
-end
+#capitalize the first and last names in a new array, and label the new array
+cap_name_array = name_array.map {|name| name.capitalize}
 
-p name_to_array(real_name)
 
-p name_to_array(real_name).last
-p name_to_array(real_name).first
+#re-arrange the new array by adding the last name into a new array, and then add the first name.
+#The result is reversing the first and last names
+reverse_name = []
+reverse_name << cap_name_array.last
+reverse_name << cap_name_array.first
+p reverse_name
 
-#exchange the order of the array
-#make the first item in the array the last item and the last item the first 
 
 
 

5d6c9130c393d0164a2d853202218b7ed0166a2d
Jack Dagley <jack.dagley77@gmail.com>
2016-11-03 08:40:20 -0700

    split array into individual characters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -22,29 +22,33 @@ def name_swap(real_name = "john doe")
 #make the user input into an array by splitting the space between first and last names,
 #and then name it to make it more clear
 name_array = real_name.split(' ')
-
-
 #capitalize the first and last names in a new array, and label the new array
 cap_name_array = name_array.map {|name| name.capitalize}
-
-
 #re-arrange the new array by adding the last name into a new array, and then add the first name.
 #The result is reversing the first and last names
 reverse_name = []
 reverse_name << cap_name_array.last
 reverse_name << cap_name_array.first
-
 end
 
 p name_swap(real_name)
 
 
+#use the array from name swap, iterate through each letter using first .each method,
+#and then the .next method
+#and then add 1 to the index of the vowels variable
+#and pass the new vowel back into the intial array
 
+#convert name array to an array of individual letters (so i can iterate through them)
+
+
+name_string = name_swap(real_name).join
+p name_string.split("")
 
 
 def next_vowel
-	vowels = "aeiou"
-	vowels.next
+	vowels.each {|i| i.next}
+
 
 end 
 

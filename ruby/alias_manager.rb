431ff0296bfc4d2b6c450006f0fcdc4537cd59e0
Jack Dagley <jack.dagley77@gmail.com>
2016-11-03 17:04:17 -0700

    thurs final

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,7 +17,7 @@ PSEUDOCODE
 puts "what is the name of the spy?"
 real_name = gets.chomp
 
-def name_swap(real_name = "john doe")
+def name_swap(real_name)
 #pass in a real name to the method, separate first and last name and turn into an array:
 #make the user input into an array by splitting the space between first and last names,
 #and then name it to make it more clear
@@ -31,35 +31,31 @@ reverse_name << cap_name_array.last
 reverse_name << cap_name_array.first
 end
 
-p name_swap(real_name)
+name_swap(real_name)
 
 
-#use the array from name swap, iterate through each letter using first .each method,
-#and then the .next method
-#and then add 1 to the index of the vowels variable
-#and pass the new vowel back into the intial array
-
-#convert name array to an array of individual letters (so i can iterate through them)
 name_string = name_swap(real_name).join
 p name_string
+#pass in name_string which will be the string of the name
+#turn this string into an array and call it new_name_array
+#make a new array using .map, which looks through each letter in new_name_array
+#and checks if that letter exists in the vowels array
+#then use the index for that letter in the vowels array
+#and apply it to the rotated vowels array so that it prints the next vowel
+#if the letter in new_name_array does not exist in the vowels array
+#add the letter to the new_name_array
+
+
+def vowel_advance(name_string)
 name_split = name_string.split('')
-p name_split #name of the new name array that is made up of individual letters
-
-#join the array back together
-
-
-#write a method so the vowels array gets mapped to another array that adds
-#an index to the argument
-
 vowels = ['a', 'e', 'i', 'o', 'u']
-p vowels
+
 new_name_array = name_split.map do |letter| if vowels.include?(letter)
-p vowels.rotate(1)[vowels.index(letter)]
-else p letter
+vowels.rotate(1)[vowels.index(letter)]
+else letter
 end
-puts new_name_array
+	p new_name_array
 
-
- #create a new array by going through each letter of the name array
-	 #if any letter in the name array is included in the vowel array
-	 end	
+end
+end
+p vowel_advance("test")

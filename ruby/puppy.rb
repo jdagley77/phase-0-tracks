8bb8d43ebf3969925ddce38874f64bf375e76817
Jack Dagley <jack.dagley77@gmail.com>
2016-11-08 07:54:03 -0800

    initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+CLASS: puppy ----------
+
+CHARACTERISTICS: ----------
+leg count: 4
+friendly: yes
+nose color: brown
+soft fur: yes
+species: varies
+color: varies
+
+BEHAVIOR: ----------
+jump 
+run
+bark
+eat
+
+end

08cdd24c93c20d92395d1603ead20fd4e297280f
Jack Dagley <jack.dagley77@gmail.com>
2016-11-17 14:24:38 -0800

    add initial pseudocode and first variables

---
+++ js/algos.js
@@ -0,0 +1,26 @@
+//In algos.js, write a function that takes an array of words or phrases and returns 
+//the longest word or phrase in the array. So if we gave your function the array of 
+//["long phrase","longest phrase","longer phrase"], it would return "longest phrase". 
+//This is a logical reasoning challenge, so try to reason through this based on what you already know, 
+//rather than Googling the fanciest solution or looking up built-in sorting functions. 
+//Note that "some string".length will do what you expect in JS.
+//Add driver code that tests your function on a few arrays.
+
+//create an array of different words
+//iterate through the items in the array
+//for each item (string) in the array, count the number of letters in each item
+//create an empty hash
+//for each string in the array, store it as a key in the object
+//store each integer as a value in an object
+//how can i sort the items of the hash by their values?
+//return the last value in the hash
+
+
+var words = ["one", "two", "three", "four", "five", "six", "seven"];
+
+var wordObj = {}
+
+for (var i = 0; i < words.length; i++) {  //iterate through the array of words
+	i.length
+
+};

d47bb10a155b74ba8fe190c105c8e72636c76a58
Jack Dagley <jack.dagley77@gmail.com>
2016-11-23 18:15:25 -0800

    add the add item method

--- databases/xmas_list/xmas_list.rb
+++ databases/xmas_list/xmas_list.rb
@@ -10,6 +10,7 @@ require 'sqlite3'
 
 db = SQLite3::Database.new("list.db")
 
+
 create_new_list = <<-SQL
   CREATE TABLE IF NOT EXISTS list(
     id INTEGER PRIMARY KEY,
@@ -20,3 +21,47 @@ create_new_list = <<-SQL
     purchased BOOLEAN
   )
 SQL
+
+db.execute(create_new_list)
+
+def add_item(db, first_name, interests, age, gift, purchased=false)
+	new_item = "INSERT INTO list (name, interests, age, gift, purchased) 
+		VALUES (?, ?, ?, ?, ?)", [first_name, interests, age, gift, purchased]
+	db.execute(new_item)
+end
+
+
+
+=begin
+def view_list 
+	<<-SQL 
+	SELECT * FROM list.db
+	SQL
+end
+=end
+
+#ask user questions and then add the data to the list table when done
+=begin
+puts "How many people are in your immediate family?"
+fam_number = gets.chomp.to_i
+
+list_count = 0
+
+until list_count == fam_number
+
+puts "Please enter the first name of the person you are getting a gift for."
+first_name = gets.chomp
+
+puts "Please enter the main interest of the person you are getting a gift for."
+interests = gets.chomp
+
+puts "Please enter the age of the person you are getting a gift for."
+age = gets.chomp
+
+puts "Please enter a gift that comes to mind for this person."
+gift = gets.chomp
+
+add_item(db, first_name, interests, age, gift, purchased=false)
+end
+=end
+

3c8e6fe4fe9f9c3a94cc579ec38347c08ecfad63
Jack Dagley <jack.dagley77@gmail.com>
2016-12-01 15:12:07 -0800

    add search route

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -27,6 +27,7 @@ end
 # all student data
 get '/students' do
   students = db.execute("SELECT * FROM students")
+  p students
   response = ""
   students.each do |student|
     response << "ID: #{student['id']}<br>"
@@ -42,7 +43,11 @@ end
 
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  show = ""
   student.to_s
+  p student
+  show << "Name: #{student['name']}</br>"
+  show << "ID: #{student['id']}"
 end
 
 get '/contact' do
@@ -57,3 +62,31 @@ get '/great_job/' do
     "Good job!"
   end
 end
+
+#take each number, turn them to integers
+#and add them
+#return the result
+
+get '/add_numbers/' do
+  first_number = params[:first_number]
+  second_number = params[:second_number]
+  result = params[:first_number].to_i + params[:second_number].to_i
+
+  "The results of #{first_number} + #{second_number} is #{result}"
+end
+
+=begin
+Optional bonus: Make a route that allows the user to search the database in some way -- 
+maybe for students who have a certain first name, or some other attribute. 
+If you like, you can simply modify the home page to take a query parameter, 
+and filter the students displayed if a query parameter is present.
+=end
+
+get '/find/:age' do
+  students = db.execute("SELECT * FROM students WHERE age=?", [params[:age]])[0]
+  p students
+  
+  "NAME: #{students['name']}</br>
+  AGE: #{students['age']}"
+end
+
